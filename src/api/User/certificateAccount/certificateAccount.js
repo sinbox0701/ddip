588ea5e7be362ce2b8e7b50546651966143dbb65
sinbox0701 <psh090953@gmail.com>
2020-11-16 19:33:06 +0900

    Edit Resolvers

--- src/api/User/certificateAccount/certificateAccount.js
+++ src/api/User/certificateAccount/certificateAccount.js
@@ -9,17 +9,33 @@ export default {
             //생성한 난수를 String으로 바꿔서 저장
             //console.log(certification);
             try{
+                const exists = await prisma.$exists.phone({
+                    OR: [
+                        {tel}
+                    ]
+                });
                 await sendMessage(tel,certification);
                 //메세지 보내기
-                await prisma.createPhone({
-                    tel,
-                    certification
-                })
+                if(exists){
+                    //console.log("exist");
+                    //console.log(tel);
+                    await prisma.updatePhone({
+                        where:{tel:tel},
+                        data:{certification}
+                    });
+                }
+                else{
+                    await prisma.createPhone({
+                        tel,
+                        certification
+                    });
+                }
                 //핸드폰 번호 + 인증 번호 저장 
                 return true;
             }
             catch(error){
-                //console.log(error);
+                console.log("here");
+                console.log(error);
                 return false;
             }
         }
--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -1,10 +1,10 @@
 import { prisma } from "../../../../generated/prisma-client";
-import * as bcrypt  from "bcryptjs";
+import * as bcrypt from "bcryptjs";
 
-export default{
-    Mutation:{
-        createAccount: async (_,args) => {
-            const{
+export default {
+    Mutation: {
+        createAccount: async (_, args) => {
+            const {
                 username,
                 password,
                 email,
@@ -17,43 +17,35 @@ export default{
 
             const exists = await prisma.$exists.user({
                 OR: [
-                    {username},
-                    {email}
+                    { username },
+                    { email }
                 ]
             });
             //user의 username과 이메일은 중복 X
-            if(exists){
+            if (exists) {
                 throw Error("Already Having Email Or Username");
             }
             // await sendMessage(tel,certificationNum);
-            try{
-                const hashedPassword = await bcrypt.hash(password,5);
-                //비밀번호 암호화
-                const certificationN = await prisma.phones({
-                    where:{tel}
+            const hashedPassword = await bcrypt.hash(password, 5);
+            //비밀번호 암호화
+            const certificationN = await prisma.phones({
+                where: { tel }
+            });
+            //입력한 폰번호를 가지고 있는 phone Db의 Instance 불러옴
+            console.log("aaaa");
+            //console.log(certificationN[0].certification);
+            console.log(certificationN[0].certification);
+            if (certification === certificationN[0].certification) {//입력 인증번호와 불러온 Instance의 인증번호가 같다면
+                await prisma.createUser({
+                    username,
+                    password: hashedPassword,
+                    email,
+                    gender,
+                    tel,
+                    certification: certificationN[0].certification
                 });
-                //입력한 폰번호를 가지고 있는 phone Db의 Instance 불러옴
-                //console.log("aaaa");
-                //console.log(certificationN[0].certification);
-                if(certification === certificationN[0].certification){//입력 인증번호와 불러온 Instance의 인증번호가 같다면
-                    const newUser = await prisma.createUser({
-                        username,
-                        password:hashedPassword,
-                        email,
-                        gender,
-                        tel,
-                        certification:certificationN[0].certification
-                    });
-                    //새로운 유저 생성
-                    //console.log(newUser);
-                    return newUser;
-                }
-                else{
-                    throw Error("Error certification");
-                }
-            }catch(error){
-                throw Error("Error createAccount");
-                //console.log(error);
+                //새로운 유저 생성
+                return true;
             }
         }
     }
